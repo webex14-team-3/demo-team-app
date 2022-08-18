552f97e9393a722abe81f8a3ebc33cbf2c69d229
Github <Github>
2022-08-18 21:29:34 +0900

    test

--- src/views/QuizApp.vue
+++ src/views/QuizApp.vue
@@ -1,43 +1,71 @@
-<templete>
-<div class="allContainer">
-  <h1>Q.{{ quiz.text }}</h1>
-  <img class="img" v-bind:src="quizImg" v-bind:alt="quiz.text"/>
-  <div class="container">
-  <button @click="pushButton(choice)" v-for="(choice, i) in quiz.choices" :key="i" >
-    {{ choice.text }}
-  </button>
+<template>
+  <div class="allContainer">
+    <h1>Q.{{ quiz.text }}</h1>
+    <img class="img" v-bind:src="quizImg" v-bind:alt="quiz.text" />
+    <div class="container">
+      <button
+        @click="pushButton(choice)"
+        v-for="(choice, index) in quiz.choices"
+        :key="index"
+      >
+        {{ choice.text }}
+      </button>
+    </div>
+    <div>{{ feedback }}</div>
   </div>
-  <div>{{ feedback }}</div>
-</div>
-</templete>
+</template>
 
 <script>
 export default {
   data() {
     return {
-      feedback: "aiueo",
-      img: "Ganymede.jpg",
-      quiz: {
-        text: "この星の名前は何でしょう？",
-        img: "Ganymede.jpg",
-        choices: [
-          {
-            text: "ゴリアテ",
-            isCorrect: false,
-            feedback: "no",
-          },
-          {
-            text: "ゼニガメ",
-            isCorrect: false,
-            feedback: "no",
-          },
-          {
-            text: "ガニメデ",
-            isCorrect: true,
-            feedback: "yes",
-          },
-        ],
-      },
+      name: "",
+      feedback: "",
+      img: "@/views/images/Ganymede.jpg",
+      quiz: [
+        {
+          text: "この星の名前は何でしょう？",
+          img: "Ganymede.jpg",
+          choices: [
+            {
+              text: "ゴリアテ",
+              isCorrect: false,
+              feedback: "no",
+            },
+            {
+              text: "ゼニガメ",
+              isCorrect: false,
+              feedback: "no",
+            },
+            {
+              text: "ガニメデ",
+              isCorrect: true,
+              feedback: "yes",
+            },
+          ],
+        },
+        {
+          text: "数字は？",
+          img: "Two.jpeg",
+          choices: [
+            {
+              text: "１",
+              isCorrect: false,
+              feedback: "no",
+            },
+            {
+              text: "３",
+              isCorrect: false,
+              feedback: "no",
+            },
+            {
+              text: "２",
+              isCorrect: true,
+              feedback: "yes",
+            },
+          ],
+        },
+      ],
     }
   },
   methods: {
@@ -48,5 +76,21 @@ export default {
       }
     },
   },
+  computed: {
+    quizImg(imgInd) {
+      return require("./images/" + this.quiz[imgInd].img)
+    },
+  },
 }
 </script>
+
+<style scoped>
+.allContainer {
+  text-align: center;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+}
+</style>
