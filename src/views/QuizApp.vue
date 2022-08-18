b66b5b9bdb3764eaa928dab8b79e9854aeddd66a
Github <Github>
2022-08-18 20:55:53 +0900

    test

--- src/views/QuizApp.vue
+++ src/views/QuizApp.vue
@@ -1,13 +1,52 @@
 <templete>
 <div class="allContainer">
-  <h1>Q.{{ quiz.title }}</h1>
-  <img class="img" v-bind:src="quizImg" v-bind:alt="quiz.title"/>
+  <h1>Q.{{ quiz.text }}</h1>
+  <img class="img" v-bind:src="quizImg" v-bind:alt="quiz.text"/>
   <div class="container">
-   <button @click="pushButton" v-for="(choice, i) in quiz.choices" :key="i" >
-    {{ quiz.choices.text }}
-   </button>
+  <button @click="pushButton(choice)" v-for="(choice, i) in quiz.choices" :key="i" >
+    {{ choice.text }}
+  </button>
   </div>
+  <div>{{ feedback }}</div>
 </div>
 </templete>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      feedback: "aiueo",
+      img: "Ganymede.jpg",
+      quiz: {
+        text: "この星の名前は何でしょう？",
+        img: "Ganymede.jpg",
+        choices: [
+          {
+            text: "ゴリアテ",
+            isCorrect: false,
+            feedback: "no",
+          },
+          {
+            text: "ゼニガメ",
+            isCorrect: false,
+            feedback: "no",
+          },
+          {
+            text: "ガニメデ",
+            isCorrect: true,
+            feedback: "yes",
+          },
+        ],
+      },
+    }
+  },
+  methods: {
+    pushButton(choice) {
+      this.feedback = choice.feedback
+      if (choice.isCorrect) {
+        console.log("yes")
+      }
+    },
+  },
+}
+</script>
